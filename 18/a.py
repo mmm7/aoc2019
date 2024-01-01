a63c840913315df23d3595da8126ed3592e35882
Mate <mate@noreply.com>
2024-01-01 18:15:10 +0100

    day18 simplify pathfinding

--- 18/a.py
+++ 18/a.py
@@ -29,35 +29,31 @@ for y in range(ys):
 print(POS)
 print(KEYS)
 
-def getpath(k,l):
-  s,t  = POS[k], POS[l]
+PATHS = defaultdict(dict)
+
+def floodfrom(k):
+  if not k in POS: return
+  s  = POS[k]
   vis = set()
   # (y,x), steps, doors
   bfs = deque([(s, 0, set())])
   while bfs:
     pos, steps, doors=bfs.popleft()
     doors = doors.copy()
-    if pos == t: return steps, doors
     if pos in vis: continue
     vis.add(pos)
     y,x = pos
     c = I[y][x]
     if c == '#': continue
     if c.isupper(): doors.add(c.lower())
+    if c.islower():
+      if steps != 0:
+        PATHS[k][c] = (steps, doors)
     for dx,dy in ((1,0), (-1,0), (0,1), (0,-1)):
       bfs.append(((y+dy, x+dx), steps+1, doors,))
-  assert False, (k,l,s,t,doors,vis)
-
-PATHS = defaultdict(dict)
 
 for k in KEYS.union(set('@')):
-  for l in KEYS:
-    if l in PATHS[k]: continue
-    if k == l: continue
-    a,b = sorted([k,l])
-    path = getpath(a,b)
-    PATHS[a][b] = path
-    PATHS[b][a] = path
+  floodfrom(k)
 
 print('PATHS', PATHS)
 

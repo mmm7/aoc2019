7bf7f856a16342a4833d64e8cc2cfa3c25129799
Mate Szalay <mate@noreply.com>
2025-01-07 16:56:27 +0100

    day14b

--- 14/14.py
+++ 14/14.py
@@ -5,26 +5,46 @@ from input import I,G
 print(I)
 
 #print(G)
-ts = list(TopologicalSorter(G).static_order())
+TS = tuple(TopologicalSorter(G).static_order())
 #print(ts)
 
-assert ts[-1]=='FUEL',ts
-assert ts[0]=='ORE',ts
+assert TS[-1]=='FUEL',TS
+assert TS[0]=='ORE',TS
 
-req=defaultdict(int)
-req['FUEL']=1
 
-while ts:
+def orenum(fuelnum):
+ req=defaultdict(int)
+ req['FUEL']=fuelnum
+ ts=list(TS)
+ while ts:
   tmat=ts.pop()
   need=req[tmat]
-  print(f'{tmat=},{need=}')
+  #print(f'{tmat=},{need=}')
   assert need>0
   if tmat=='ORE': break
   tnum,ss = I[tmat]
   mult = (need+tnum-1)//tnum
-  print(f'{tnum=},{mult=},{ss=}')
+  #print(f'{tnum=},{mult=},{ss=}')
   assert mult*tnum>=need
   for snum,smat in ss:
     req[smat]+=snum*mult
+ return req['ORE']
 
-print('A:', req['ORE'])
+asol=orenum(1)
+print('A:', asol)
+
+TARGET=1000000000000
+l,h=1,TARGET*asol
+
+while l+1<h:
+  assert orenum(l)<TARGET
+  assert orenum(h)>=TARGET
+  m = (l+h)//2
+  ore=orenum(m)
+  if ore<TARGET:
+    l=m
+  else:
+    h=m
+
+assert l+1==h, (l,h)
+print('B:',l)
